8c53ffad6372fab697fdd964929d2522bbd295d3
bmsubra25 <bmsubra25@gmail.com>
2026-08-10 16:27:33 -0700

    Update qu_fem.py

--- qu_fem.py
+++ qu_fem.py
@@ -386,7 +386,7 @@ def generate_c_j_array(j, nodal_functions, gauss_tuples):
   for i in range(len(gauss_tuples)):
     point = list(gauss_tuples[i][0:len(gauss_tuples[i])-1])
     weight = gauss_tuples[i][-1]
-    coeff_array.append(weight * nodal_functions[j](*point))
+    coeff_array.append(weight * np.real_if_close(nodal_functions[j](*point)))
   return coeff_array
 
 """Function Operator LCU"""
