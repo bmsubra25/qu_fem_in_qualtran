dccfb3dcd66ecad3bb05c5de78c95e67f8aab9f6
bmsubra25 <bmsubra25@gmail.com>
2026-08-10 17:07:37 -0700

    Update qu_fem.py

--- qu_fem.py
+++ qu_fem.py
@@ -420,7 +420,7 @@ def construct_source_vector_diag(G, nen_1D, numel, numnp, numnp_bits, d, nodal_b
     sum_functions = generate_function_operator_lcu_diag(G, numel, numnp, numnp_bits, d, j, nodal_basis_functions, source_function)
     print(f"a_j.system_bitsize {a_j.system_bitsize}" )
     print(f"sum_functions.system_bitsize {sum_functions.system_bitsize}")
-      print(f"a_k_adj.system_bitsize {AdjointBlockEncoding(a_j).system_bitsize}" )
+    print(f"a_k_adj.system_bitsize {AdjointBlockEncoding(a_j).system_bitsize}" )
     block_encodings.append(BlockEncodingProduct((a_j_be, sum_functions, AdjointBlockEncoding(a_j))))
   coeffs = [1.0 for _ in range(len(block_encodings))]
   return LinearCombination(block_encodings = tuple(block_encodings),lambd = tuple(coeffs),lambd_bits = 5)
