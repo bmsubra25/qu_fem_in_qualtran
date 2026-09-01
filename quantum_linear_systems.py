74f06783997dcc5ed3e858720e1ce986972277fe
bmsubra25 <bmsubra25@gmail.com>
2026-08-31 19:21:42 -0500

    Update quantum_linear_systems.py

--- quantum_linear_systems.py
+++ quantum_linear_systems.py
@@ -202,7 +202,7 @@ class QSVT_Adj_Im(BlockEncoding):
 """QSVT(Approximation of c/x on singular values)"""
 
 def return_qsvt(M):
-  coeffs, scale = pyqsp.poly.PolyOneOverX().generate(return_scale = True, kappa = 2, epsilon = 0.5)
+  coeffs, scale = pyqsp.poly.PolyOneOverX().generate(return_scale = True, kappa = 2, epsilon = 1.0)
   angles_qsp = pyqsp.angle_sequence.QuantumSignalProcessingPhases(coeffs, method = "laurent", signal_operator = "Wx")
   angles_qsvt = qml.transform_angles(angles_qsp, "QSP","QSVT")
   return QSVT(M, angles_qsvt)
